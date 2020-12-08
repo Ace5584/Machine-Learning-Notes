d9c75a65d4bae11177b045a2a2207553fc3afd24
Alex lai <aexace666@gmail.com>
2020-12-08 13:10:55 +1100

    Created week 3 code and added code for convolutional images

---
+++ DeepLearning AI/Introduction to TF/Code/Week 3/Convolution images/main.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import models
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.95):
+      print("\nReached 90% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callback = myCallback()
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+training_images=training_images.reshape(60000, 28, 28, 1)
+training_images=training_images / 255.0
+
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images=test_images/255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=10, callbacks=[callback])
+test_loss = model.evaluate(test_images, test_labels) 
+
+f, axarr = plt.subplots(3,4)
+FIRST_IMAGE=3
+SECOND_IMAGE=4
+THIRD_IMAGE=5
+CONVOLUTION_NUMBER = 1
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
+plt.show()
+
